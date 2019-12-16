6c8a0e05da7cce5a448df9965cca83f787ec211b
openlab <openlab@Z00P1-20.SPSKNM>
2019-12-16 09:39:37 +0000

    Add project files.

---
+++ BPM/BPM/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BPM
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private readonly List<DateTime> tapTimes;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            tapTimes = new List<DateTime>();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            tapTimes.Add(DateTime.Now);
+            if (tapTimes.Count > 2)
+            {
+                var oldest = tapTimes.First();
+                var newest = tapTimes.Last();
+                var duration = newest - oldest;
+                var average = new TimeSpan(duration.Ticks / tapTimes.Count);
+                double bpm = 60 / average.TotalSeconds;
+                bpmLabel.Text = Math.Round(bpm).ToString();
+
+            }
+        }
+    }
+}
